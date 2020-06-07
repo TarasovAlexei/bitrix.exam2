b3e7455791dc63fa96d1e996590a3efb05781232
Alexei Tarasov <alexeitara5ov@yandex.rualexeitara5ov@yandex.ru>
2020-06-08 00:25:00 +0300

    [ex2-51] Изменение данных в письме

--- local/php_interface/include/handlers.php
+++ local/php_interface/include/handlers.php
@@ -1,9 +1,29 @@
 <?
 AddEventHandler('main', 'OnEpilog', array("ExamHandlers", "Check404Error"));
-
+AddEventHandler("main", "OnBeforeEventAdd", array("ExamHandlers", "OnBeforeEventAddHandler"));
 
 class ExamHandlers
-{
+{   
+    function OnBeforeEventAddHandler(&$event, &$lid, &$arFields){
+        if($event == 'FEEDBACK_FORM'){
+            global $USER;
+            if($USER->isAuthorized()){
+                $arFields['AUTHOR'] = 'Пользователь авторизован: ' . $arUser['ID'] . ' ('. $arUser['LOGIN'] .') '
+                . $arUser['NAME'] . ', данные из формы: ' . $arFields["AUTHOR"];
+            }
+            else{
+                $arFields['AUTHOR'] = 'Пользователь не авторизован, данные из формы: ' . $arFields["AUTHOR"];
+            }
+
+            CEventLog::Add(array(
+            "SEVERITY" => "INFO",
+            "AUDIT_TYPE_ID" => "MY_TYPE_LOG",
+            "MODULE_ID" => "main",
+            "ITEM_ID" => $arFields['ID'],
+            "DESCRIPTION" => "Замена данных в отсылаемом письме – " . $arFields['AUTHOR'],
+            );
+        }
+    }
 
     // ex2-94
 	function Check404Error(){
